f0393f201a123c99360f6a34d8067860a9a4aab3
Janis Miezitis <janjiss@gmail.com>
2015-01-07 15:30:26 +0200

    Fixed initialization code

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,4 @@
+platform = RUBY_PLATFORM
 if platform.include?("darwin") # OS X machine 
   binary_path = Rails.root.join('bin', 'wkhtmltopdf-0.9.9-OS-X-i386').to_s
 elsif platform.include?("64-linux") # 64-bit linux machine 
