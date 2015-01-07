5809e5c662f46844465a4f954be28a2276228456
Janis Miezitis <janjiss@gmail.com>
2015-01-07 14:42:15 +0200

    Added wickedpdf config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,3 @@
 WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/home/alexandr/.rvm/gems/ruby-2.1.2/bin/wkhtmltopdf'
+  :exe_path => '/usr/local/bin/wkhtmltopdf'
 }
