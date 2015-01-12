8bd4cb757c9853d0e7459ad1c1dff37f5977d5eb
Lauris Bligzna <bligzna.lauris@gmail.com>
2015-01-12 17:06:52 +0200

    render json

--- app/controllers/postcards_controller.rb
+++ app/controllers/postcards_controller.rb
@@ -2,85 +2,25 @@ class PostcardsController < ApplicationController
   respond_to :json
 
   def index
-    render json: Receiver.limit(5)
+    render json: Receiver.all
   end
   
   def create
-    binding.pry
-    render json: Receiver.create(name: params[:name])
+    render json: Receiver.create(postcard_params)
   end
 
-  # def update
-  #   render json:  Receiver.update(safe_params[:id], safe_params)
-  # end
+  def update
+    render json:  Receiver.update(postcard_params)
+  end
 
-  # def destroy
-  #   render json:  Receiver.destroy(params[:id])
-  # end
-
-  # def index
-  #   @receivers = Receiver.limit(5)
-  # end
-
-  # def new
-  #   if params[:id]
-  #     @receiver = Receiver.find(params[:id])
-  #   else
-  #     @receiver = Receiver.new
-  #   end
-  # end
-
-  # def create
-  #   @receiver = Receiver.where(
-  #         first_name: params[:receiver][:first_name],
-  #         last_name: params[:receiver][:last_name],
-  #         address_line_1: params[:receiver][:address_line_1],
-  #         account_id: current_account.id
-  #         ).first_or_create(postcard_params)
-  #   redirect_to action: 'index', id: @receiver.id
-  # end
-
-  # def update_receiver
-  #   @receiver = Receiver.find(params[:receiver][:id])
-  #   @receiver.update_attributes(postcard_params)
-  #   redirect_to action: 'show', id: @receiver.id
-  # end
-
-  # def show
-  #   @receiver = Receiver.find(params[:id])
-  #   mm_in_inch = 25.4
-  #   page_height = mm_in_inch*5.5
-  #   page_width = mm_in_inch*8.5
-  #   render  pdf: "postcard_#{params[:id]}",
-  #           template: 'postcards/show.html.haml',
-  #           layout: 'pdf',
-  #           page_width: page_width,
-  #           page_height: page_height
-  # end
-
-  # def receivers
-  #   if !params[:first_name].empty? || !params[:last_name].empty?
-  #     if !params[:first_name].empty?
-  #       @receivers = Receiver.where("(first_name LIKE ?)", "%#{params[:first_name]}%")
-  #     else
-  #       @receivers = Receiver.all
-  #     end
-
-  #     if !params[:last_name].empty?
-  #       @receivers = @receivers.where( "(last_name LIKE ?)", "%#{params[:last_name]}%")
-  #     end
-  #   end
-
-  #   if @receivers.empty?
-  #     render json: []
-  #   else
-  #     render json: @receivers.to_json
-  #   end
-  # end
+  def destroy
+    render json:  Receiver.destroy(params[:id])
+  end
 
   private
 
   def postcard_params
-    params.require(:receiver).permit(:first_name, :last_name, :zip, :city, :country, :address_line_1, :address_line_2, :address_line_3)
+    params.require(:postcard).permit(:first_name, :last_name, :zip, :city, :country, :address_line_1, :address_line_2, :address_line_3)
   end
+
 end
