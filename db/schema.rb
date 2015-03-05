037f8681c1ee496a7fcaa91e0b5f05a0c47ea05e
Lauris Bligzna <bligzna.lauris@gmail.com>
2015-03-05 14:52:17 +0200

    scjema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304134709) do
+ActiveRecord::Schema.define(version: 20150305104326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20150304134709) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_admin",               default: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true, using: :btree
