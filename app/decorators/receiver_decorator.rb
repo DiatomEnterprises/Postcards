6262a99ddd0074b5ed37ef091a841923e94a709e
DainisL <dainis186@gmail.com>
2015-04-17 13:00:46 +0300

    fix

--- app/decorators/receiver_decorator.rb
+++ app/decorators/receiver_decorator.rb
@@ -8,10 +8,10 @@ class ReceiverDecorator < Draper::Decorator
 
   def full_location(city: true, state: true, country: true, zip: true)
     location = []
-    location << receiver.city if !receiver.city.present? && city
-    location << receiver.state if !receiver.state.present? && state
-    location << receiver.country if !receiver.country.present? && country
-    location << receiver.zip if !receiver.zip.present? && zip
+    location << receiver.city if receiver.city.present? && city
+    location << receiver.state if receiver.state.present? && state
+    location << receiver.country if receiver.country.present? && country
+    location << receiver.zip if receiver.zip.present? && zip
     location.join(', ')
   end
 end
